5121e4d38bc12775b9fc864923fd728e05c37bab
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2024-07-21 12:43:05 +0330

    added game_over screen and restart

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from sprites import Background, Ground, Bird, Pipe
 
 class Game():
     def __init__(self):
+        # Game setup
         pygame.init()
         self.display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Flappy Bird!")
         self.clock = pygame.time.Clock()
+        self.active = True
+        self.start_offset = 0
 
+        # Sprites
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
 
@@ -19,45 +23,65 @@ class Game():
         self.gound = Ground([self.all_sprites, self.collision_sprites])
         self.bird = Bird(self.all_sprites, scale_factor=1.5)
 
+        # Timer
         self.pipe_timer = pygame.USEREVENT + 1
         pygame.time.set_timer(self.pipe_timer, 1400)
 
-        self.font = pygame.font.SysFont('arial', 30)
+        # Text
+        self.font = pygame.font.SysFont('arial', 28, True)
         self.score = 0
     
     def collisions(self):
         if pygame.sprite.spritecollide(self.bird, self.collision_sprites, False, pygame.sprite.collide_mask)\
             or self.bird.rect.top <= 0:
-            pygame.quit()
-            sys.exit()
+            self.active = False
+            for sprite in self.collision_sprites:
+                if isinstance(sprite, Pipe):
+                    sprite.kill()
+            self.bird.kill()
 
     def display_score(self):
-        self.score = pygame.time.get_ticks() // 100
+        if not self.active:
+            return
+        self.score = (pygame.time.get_ticks() - self.start_offset) // 200
         score_surf =  self.font.render(str(self.score), True, "white")
         score_rect = score_surf.get_rect(midtop=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 10))
         self.display.blit(score_surf, score_rect)
-
+        
     def run(self):
         last_time = time.time()
         while True:
+            # Event loop
             for event in pygame.event.get():
                 if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
-                    self.bird.jump()
+                    if self.active:
+                        self.bird.jump()
+                    else:
+                        self.active = True
+                        self.start_offset = pygame.time.get_ticks()
+                        self.bird = Bird(self.all_sprites, scale_factor=1.5)
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                if event.type == self.pipe_timer:
-                    Pipe([self.all_sprites, self.collision_sprites], scale_factor=1.1)
+                if event.type == self.pipe_timer and self.active:
+                    Pipe([self.all_sprites, self.collision_sprites], scale_factor=1.08)
             
             dt = time.time() - last_time
             last_time = time.time() 
             
             # Game logic
             self.display.fill("black")
-            self.collisions()
             self.all_sprites.update(dt)
             self.all_sprites.draw(self.display)
             self.display_score()
+
+            if self.active:
+                self.collisions()
+            else:
+                go_surf =  pygame.font.SysFont('timesnewroman', 70, True).render("Game Over", True, (235, 58, 14))
+                go_rect = go_surf.get_rect(midtop=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+                self.display.blit(go_surf, go_rect)
+
             pygame.display.update()
             self.clock.tick(FPS)
 
